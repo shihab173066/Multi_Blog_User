87130ec33661ca96a54e9b20ae77b4539ca1d7a9
shihab173066 <shihab.hossainSH@gmail.com>
2025-08-26 03:42:46 +0600

    Serializers and writing logic for registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from rest_framework import status
+
 from django.shortcuts import render
 
-# Create your views here.
+def RegisterView(APIView):
+
+    def post(self, request):
+        try:
+            data= request.data
+            
+            serializer = RegisterSerializer(data = data)
+
+            if not serializer.is_valid():
+               return Response({
+                  'data' : serializer.errors,
+                  'message' : 'something went wrong'
+               }, status =  status.HTTP_400_BAD_REQUEST)
+            
+
+            serializer.save()
+
+            return Response({
+                'data' : {},
+                'message' : 'your account is created'
+            }, status = status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as e:
+            return Response({
+                'data' : {},
+                'message' : 'something went wrong' 
+            }, status =  status.HTTP_400_BAD_REQUEST)
